95e4846770367144cef34cbb3628ba2925156f71
mathias-silva <mathiassilvasantos2016@gmail.com>
2025-11-12 14:18:37 -0300

    endpoint com parametros de rota

--- app.js
+++ app.js
@@ -20,6 +20,17 @@ servidor.get('/mensagem/ocupado/recado',(req,resp)=>{
     resp.send("Estou ocupado no momento, deixe uma mensagem no email contato@meusite.com")
 });
 
+
+
+servidor.get('/calculadora/soma/:n1/:n2',(req,resp)=>{
+    const n1 = Number(req.params.n1);
+    const n2 = Number(req.params.n2);
+    const soma = n1 + n2;
+    resp.send(`A soma entre ${n1} e ${n2} é igual a ${soma}`);
+})
+
+
+
 servidor.listen(5001, () => {
     console.log('Servidor rodando na porta 5001');
 });
