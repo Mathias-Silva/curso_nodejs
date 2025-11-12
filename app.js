b33baa1bbcfddbc2c1d6df2d980d21a4eaaf4a40
mathias-silva <mathiassilvasantos2016@gmail.com>
2025-11-12 14:42:25 -0300

    paramentro de query e de corpo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 const servidor = express();
+servidor.use(express.json());
 
 servidor.get('/helloworld', (req, resp) => {
     resp.send('Hello world!!');
@@ -21,7 +22,7 @@ servidor.get('/mensagem/ocupado/recado',(req,resp)=>{
 });
 
 
-
+// endpoint com parametros de rota
 servidor.get('/calculadora/soma/:n1/:n2',(req,resp)=>{
     const n1 = Number(req.params.n1);
     const n2 = Number(req.params.n2);
@@ -30,6 +31,39 @@ servidor.get('/calculadora/soma/:n1/:n2',(req,resp)=>{
 })
 
 
+
+servidor.get('/calculadora/subtrair/:n1/:n2',(req,resp)=>{
+const n1 = Number(req.params.n1);
+const n2 = Number(req.params.n2);
+const subtracao = n1 - n2;
+resp.send(`A subtração de ${n1} - ${n2} é igual a ${subtracao}`);
+})
+
+
+// endpoint com parametros de query string e number
+servidor.get('/mensagem', (req,resp)=>{
+	const nome = req.query.nome ?? 'visitante';
+    resp.send(`Olá, ${nome}!`);
+});
+servidor.get('/calculadora/somar2', (req,resp)=>{
+	const n1 = Number(req.query.n1);
+	const n2 = Number(req.query.n2);
+	const soma = n1 + n2;
+	 resp.send(`A soma entre ${n1} e ${n2} é igual a ${soma}`);
+});
+
+//endoint com parametros de corpo da requisição - POST
+
+
+servidor.post('/media', (req,resp)=>{
+	let n1 = req.body.nota1;
+	let n2 = req.body.nota2;
+	let n3 = req.body.nota3;
+		
+	let media = (n1+n2+n3) / 3;
+
+	resp.send(' A média é '+ media);
+})
 
 servidor.listen(5001, () => {
     console.log('Servidor rodando na porta 5001');
